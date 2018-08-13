b4cc8ec3858b201599f52faabdf789c5695fe2f4
valentin.kozlov <valentin.kozlov@kit.edu>
2018-08-13 14:51:36 +0200

    change src->cookiecutter.project, adapt setup.py

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='{{ cookiecutter.project_name.lower().replace(' ', '_') }}',
     packages=find_packages(),
     version='0.1.0',
     description='{{ cookiecutter.description }}',
