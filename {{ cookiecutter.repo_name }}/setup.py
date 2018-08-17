de683c127653afad56d6ad2252b0fa2e8f1b0579
valentin.kozlov <valentin.kozlov@kit.edu>
2018-08-17 16:14:27 +0200

    restructure directory, remove Makefile, add docker and Jenkinsfile

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,9 +1,9 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='{{ cookiecutter.project_name.lower().replace(' ', '_') }}',
+    name='{{ cookiecutter.repo_name }}',
     packages=find_packages(),
-    version='0.1.0',
+    version='{{ cookiecutter.app_version }}',
     description='{{ cookiecutter.description }}',
     author='{{ cookiecutter.author_name }}',
     license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}',
