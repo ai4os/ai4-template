c740128757725f96148de2d5f7d0ab7599327437
valentin.kozlov <valentin.kozlov@kit.edu>
2018-08-20 14:44:12 +0200

    debug post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,8 +4,9 @@ import shutil
 import os
 import sys
 
-dest = '../DEEPP-OC-{{ cookiecutter.repo_name }}'
-src = '../{{ cookiecutter.repo_name }}/'+ dest
+DEEP-OC = 'DEEPP-OC-{{ cookiecutter.repo_name }}'
+dest = '../' + DEEP-OC
+src = '../{{ cookiecutter.repo_name }}/'+ DEEP-OC
 
 
 try:
@@ -13,7 +14,7 @@ try:
     sys.exit(0)
 except OSError as e:
     sys.stdout.write(
-        'While attempting to move'+dest+'an error occurred'
+        'While attempting to move '+ src +' an error occurred! '
     )
-    sys.stdout.write('Error! {}'.format(e))
+    sys.stdout.write('Error! {} '.format(e))
 
