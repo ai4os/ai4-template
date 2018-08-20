25c2a69c3c7ca3d82a57545f2134dfdcc751149e
valentin.kozlov <valentin.kozlov@kit.edu>
2018-08-20 14:41:43 +0200

    debug post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,15 +4,16 @@ import shutil
 import os
 import sys
 
-src = '../{{ cookiecutter.repo_name }}/DC-OC-{{ cookiecutter.repo_name }}'
-dest = '../DEEP-OC-{{ cookiecutter.repo_name }}'
+dest = '../DEEPP-OC-{{ cookiecutter.repo_name }}'
+src = '../{{ cookiecutter.repo_name }}/'+ dest
+
 
 try:
     shutil.move(src, dest)
     sys.exit(0)
 except OSError as e:
     sys.stdout.write(
-        'While attempting to move DC-OC-{{ cookiecutter.repo_name }} an error occurred'
+        'While attempting to move'+dest+'an error occurred'
     )
     sys.stdout.write('Error! {}'.format(e))
 
