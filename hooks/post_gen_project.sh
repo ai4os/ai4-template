0762352eb942679214d5216b22a5fc556ebe0538
valentin.kozlov <valentin.kozlov@kit.edu>
2018-08-20 14:12:14 +0200

    debug post_gen_project.py

--- hooks/post_gen_project.sh
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import shutil
-import os
-src = {{ cookiecutter.repo_name }}/DC-OC-{{ cookiecutter.repo_name }}
-dest = DC-OC-{{ cookiecutter.repo_name }}
-
-shutil.move(src, dest)
