64946e20153fb8c17dafb965d26a0ecf2f58d435
valentin.kozlov <valentin.kozlov@kit.edu>
2018-08-20 14:15:16 +0200

    debug post_gen_project.py

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import shutil
+import os
+import sys
+
+src = '../{{ cookiecutter.repo_name }}/DC-OC-{{ cookiecutter.repo_name }}'
+dest = '../DC-OC-{{ cookiecutter.repo_name }}'
+
+try:
+    shutil.move(src, dest)
+    sys.exit(0)
+except OSError as e:
+    sys.stdout.write(
+        'While attempting to move DC-OC-{{ cookiecutter.repo_name }} an error occurred'
+    )
+    sys.stdout.write('Error! {}'.format(e))
+
