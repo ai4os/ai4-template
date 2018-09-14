fb1b51475b0825d37ef2860688ae775e996be573
valentin.kozlov <valentin.kozlov@kit.edu>
2018-09-14 13:29:45 +0200

    modify Jenkinsfile, attempt to make git init in post-hooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,20 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import shutil
 import os
+import shutil
+import subprocess
 import sys
 
+repo_name = '{{ cookiecutter.repo_name }}'
 deep_oc = 'DEEP-OC-{{ cookiecutter.repo_name }}'
-dest = '../' + deep_oc
-src = '../{{ cookiecutter.repo_name }}/'+ deep_oc
+deep_oc_dir = '../' + deep_oc
+src = os.path.join("../", repo_name, deep_oc)
 
+def git_ini(repo):
+    githubrepo = ("http://github.com/" + '{{ cookiecutter.github_user }}'
+                   + "/" +  '{{ cookiecutter.repo_name }}.git')
+                   
+    subprocess.run(["cd", "../" + repo])
+    subprocess.run(["git", "init"])
+    subprocess.run(["git", "add", "."])
+    subprocess.run(["git", "commit", "-m", "initial commit"])      
+    subprocess.run(["git", "remote", "add", "origin", githubrepo])        
 
+    
+    
 try:
-    shutil.move(src, dest)
+    shutil.move(src, deep_oc_dir)
+
+    git_ini(repo_name)
+    git_ini(deep_oc)
+    
     sys.exit(0)
 except OSError as e:
     sys.stdout.write(
-        'While attempting to move '+ src +' an error occurred! '
+        'While attempting to move '+ src +' and create git \
+         repository an error occurred! '
     )
     sys.stdout.write('Error! {} '.format(e))
+
 
--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/models/train_model.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-# import project config.py
-import {{ cookiecutter.repo_name }}.config as cfg
