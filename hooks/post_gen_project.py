44bd6b158940c1c12afd10c8959fead67b881a1e
valentin.kozlov <valentin.kozlov@kit.edu>
2018-10-23 17:12:15 +0200

    small polishing on code style after pylint

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+""" Post-hook script
+    Moves DEEP-OC-{{ cookiecutter.repo_name }} directory one level up.
+    Initialized Git repositories
+"""
 import os
 import shutil
 import subprocess as subp
@@ -7,36 +11,38 @@ import sys
 
 repo_name = '{{ cookiecutter.repo_name }}'
 deep_oc = 'DEEP-OC-{{ cookiecutter.repo_name }}'
-deep_oc_dir = '../' + deep_oc
+deep_oc_dir = os.path.join("../", deep_oc)
 src = os.path.join("../", repo_name, deep_oc)
 
 def git_ini(repo):
+    """ Function
+        Initializes Git repository
+    """
     githubrepo = ("git@github.com:" + '{{ cookiecutter.github_user }}'
-                   + "/" +  repo + '.git')
+                  + "/" +  repo + '.git')
     try:
         os.chdir("../" + repo)
         subp.call(["git", "init"])
         subp.call(["git", "add", "."])
-        subp.call(["git", "commit", "-m", "initial commit"]) 
+        subp.call(["git", "commit", "-m", "initial commit"])
         subp.call(["git", "remote", "add", "origin", githubrepo])
-    except OSError as e:
+    except OSError as os_error:
         sys.stdout.write('Creating git repository failed for ' + repo + " !")
-        sys.stdout.write('Error! {} '.format(e))
+        sys.stdout.write('Error! {} '.format(os_error))
 
-    
-    
+
 try:
+    # move DEEP-OC-{{ cookiecutter.repo_name }} one level up
     shutil.move(src, deep_oc_dir)
 
+    # initialized both git repositories
     git_ini(repo_name)
     git_ini(deep_oc)
-    
+
     sys.exit(0)
-except OSError as e:
+except OSError as os_error:
     sys.stdout.write(
         'While attempting to move '+ src +' and create git \
          repository an error occurred! '
     )
-    sys.stdout.write('Error! {} '.format(e))
-
-
+    sys.stdout.write('Error! {} '.format(os_error))
