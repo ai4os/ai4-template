d49c4a27e4070aa0922d9c80484191eb207ab2c4
valentin.kozlov <valentin.kozlov@kit.edu>
2019-09-07 23:53:40 +0200

    add test_unit_model.py

---
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/tests/test_unit_model.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import unittest
+import {{ cookiecutter.repo_name }}.models.deepaas_api as deepaas_api
+
+class TestModelMethods(unittest.TestCase):
+    
+    def setUp(self):
+        self.meta = deepaas_api.get_metadata()
+        
+    def test_model_metadata_type(self):
+        """
+        Test that get_metadata() returns dict
+        """
+        self.assertTrue(type(self.meta) is dict)
+        
+    def test_model_metadata_values(self):
+        """
+        Test that get_metadata() returns right values (subset)
+        """
+        self.assertEqual(self.meta['Name'].replace('-','').replace('_',''),
+                        '{{ cookiecutter.repo_name }}'.replace('-','').replace('_',''))
+        self.assertEqual(self.meta['Author'], '{{ cookiecutter.author_name }}')
+        self.assertEqual(self.meta['Author-email'], '{{ cookiecutter.author_email }}')
+
+
+if __name__ == '__main__':
+    unittest.main()
