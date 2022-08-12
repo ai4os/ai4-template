d558a42636d5c676dc01616a392ae558fc30d8fe
Ignacio Heredia <iheredia@ifca.unican.es>
2022-08-12 13:38:59 +0200

    add tensorboard to misc utils

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/misc.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/misc.py
@@ -5,6 +5,7 @@ but users might want nevertheless to take advantage from them.
 """
 
 from functools import wraps
+from multiprocessing import Process
 import subprocess
 import warnings
 
@@ -54,10 +55,43 @@ def mount_nextcloud(frompath, topath):
 
     Example of usage:
         mount_nextcloud('rshare:/data/images', 'my_local_image_path')
+
+    Parameters
+    ==========
+    * frompath: str, pathlib.Path
+        Source folder to be copied
+    * topath: str, pathlib.Path
+        Destination folder
     """
-    command = ["rclone", "copy", frompath, topath]
+    command = ["rclone", "copy", f"{frompath}", f"{topath}"]
     result = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, error = result.communicate()
     if error:
         warnings.warn(f"Error while mounting NextCloud: {error}")
     return output, error
+
+
+def launch_cmd(logdir, port):
+    subprocess.call(["tensorboard",
+                     "--logdir", f"{logdir}",
+                     "--port", f"{port}",
+                     "--host", "0.0.0.0"])
+
+
+def launch_tensorboard(logdir, port=6006):
+    """
+    Run Tensorboard on a separate Process on behalf of the user
+
+    Parameters
+    ==========
+    * logdir: str, pathlib.Path
+        Folder path to tensorboard logs.
+    * port: int
+        Port to use for the monitoring webserver.
+    """
+    subprocess.run(
+        ["fuser", "-k", f"{port}/tcp"]  # kill any previous process in that port
+    )
+    p = Process(target=launch_cmd, args=(logdir, port), daemon=True)
+    p.start()
+
