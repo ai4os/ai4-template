6acd1ed7798a7da9e1b0176f640adbae2f8dd589
Valentin <valentin.kozlov@gmail.com>
2024-03-16 13:36:03 +0100

    feat: update how CPU / GPU tags are defined

--- {{ cookiecutter.__repo_name }}/JenkinsConstants.groovy
+++ {{ cookiecutter.__repo_name }}/JenkinsConstants.groovy
@@ -1,16 +1,28 @@
 import groovy.transform.Field
-// This file defines three variables to be used in the AI4OS-Hub Upstream Jenkins pipeline
-// base_cpu_tag : base docker image for Dockerfile, CPU version
-// base_gpu_tag : base docker image for Dockerfile, GPU version
+// This file defines variables to be used in the AI4OS-Hub Upstream Jenkins pipeline
 // dockerfile : what Dockerfile to use for building, can include path, e.g. docker/Dockerfile
+// If *both*, CPU and GPU versions can be built:
+// base_cpu_tag : CPU tag for the base docker image (check your Dockerfile)
+// base_gpu_tag : GPU tag for the base docker image (check your Dockerfile)
 
 @Field
-def base_cpu_tag = '{{ cookiecutter.baseimage_cpu_tag }}'
+def dockerfile = 'Dockerfile'
+
+{% if cookiecutter.baseimage_gpu_tag|length > 0  %}
+@Field
+def base_cpu_tag = '{{ cookiecutter.baseimage_tag }}'
 
 @Field
 def base_gpu_tag = '{{ cookiecutter.baseimage_gpu_tag }}'
 
-@Field
-def dockerfile = 'Dockerfile'
+{% else %}
+// If <docker_baseimage> has separate CPU and GPU versions
+// Uncomment following lines and define both values
+//@Field
+//def base_cpu_tag = '{{ cookiecutter.baseimage_tag }}'
+//
+//@Field
+//def base_gpu_tag = '{{ cookiecutter.baseimage_gpu_tag }}'
+{% endif %}
 
 return this;
