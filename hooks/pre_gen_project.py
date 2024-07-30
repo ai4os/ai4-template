a6be370d47f637af58267a75146929ad17867072
Valentin <valentin.kozlov@gmail.com>
2024-07-30 14:29:23 +0200

    feat: update pre_gen_project to catch all errors

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -27,22 +27,27 @@ APP_VERSION_REGEX = r"^\d+\.\d+\.\d+$"
 # -----------------------------------------------------------------------------
 def validate_git_base_url():
     """Validate git_base_url"""
-    git_base_url = urlparse(url="{{ cookiecutter.git_base_url }}")
-    if not bool(git_base_url.scheme and git_base_url.netloc):
-        logging.error("Invalid git_base_url %s", git_base_url)
-        raise ValueError(f"Invalid git_base_url : {git_base_url}")
+    git_base_url = "{{ cookiecutter.git_base_url }}"
+    parsed_url = urlparse(url=git_base_url)
+    if not bool(parsed_url.scheme and parsed_url.netloc):
+        e_message = f"Invalid git_base_url ({git_base_url})"
+        logging.error(e_message)
+        raise ValueError(e_message)
 
 
 # -----------------------------------------------------------------------------
 def validate_project_name():
     """Validate project_name"""
     project_name = "{{ cookiecutter.project_name }}"
+    e_message = []
     if len(project_name) < 2:
-        logging.error("Invalid project name (%s), length < 2", project_name)
-        raise ValueError("Invalid project name")
+        e_message = f"Invalid project name ({project_name}), length < 2 characters"
+        logging.error(e_message)
+        raise ValueError(e_message)
     if len(project_name.split(" ")) > 4:
-        logging.error("Invalid project name (%s), words > 4", project_name)
-        raise ValueError("Invalid project_name, must be 2 < project_name < 4 words")
+        e_message = f"Invalid project name ({project_name}), length > 4 words)"
+        logging.error(e_message)
+        raise ValueError(e_message)
 
 # repo_name and app_name are derived automatically in cookiecutter.json,
 # nevertheless, let's check them here
@@ -51,16 +56,18 @@ def validate_repo_name():
     """Validate repo_name"""
     repo_name = "{{ cookiecutter.__repo_name }}"
     if not re.match(FOLDER_REGEX, repo_name):
-        logging.error("Invalid characters in repo name (%s)", repo_name)
-        raise ValueError("Invalid repository parsing")
+        e_message = f"Invalid characters in repo_name ({repo_name})"
+        logging.error(e_message)
+        raise ValueError(e_message)
 
 
 def validate_app_name():
     """Validate app_name"""
     app_name = "{{ cookiecutter.__app_name }}"
     if not re.match(MODULE_REGEX, app_name):
-        logging.error("Invalid package name (%s)", app_name)
-        raise ValueError("Invalid package name parsing")
+        e_message = f"Invalid package name ({app_name})"
+        logging.error(e_message )
+        raise ValueError(e_message)
 
 # -----------------------------------------------------------------------------
 def validate_authors():
@@ -68,13 +75,15 @@ def validate_authors():
     author_emails = "{{ cookiecutter.author_email }}".split(",")
     for email in author_emails:
         if not re.match(EMAIL_REGEX, email.strip()):
-            logging.error("Invalid author_email %s", email)
-            raise ValueError("Invalid author_email")
+            e_message = f"Invalid author_email ({email})"
+            logging.error(e_message)
+            raise ValueError(e_message)
     author_names = "{{ cookiecutter.author_name }}".split(",")
     lens = n_authors, n_emails = len(author_names), len(author_emails)
     if n_emails != n_authors:
-        logging.error("Authors (%s) not matching number of emails (%s)", *lens)
-        raise ValueError("Authors not matching number of emails")
+        e_message = f"Authors ({n_authors}) not matching number of emails ({n_emails})"
+        logging.error(e_message)
+        raise ValueError(e_message)
 
 
 # -----------------------------------------------------------------------------
@@ -82,57 +91,33 @@ def validate_app_version():
     """Validate app_version"""
     app_version = "{{ cookiecutter.app_version }}"
     if not re.match(APP_VERSION_REGEX, app_version):
-        logging.error("Invalid app_version %s", app_version)
-        raise ValueError("Invalid app_version")
+        e_message = f"Invalid app_version ({app_version})"
+        logging.error(e_message)
+        raise ValueError(e_message)
 
 # -----------------------------------------------------------------------------
-# If any of the validation, exit with error
+# Run all validations, exit with error if any
 # init error_messages
 error = False
 error_messages = []
-try:
-    validate_git_base_url()
-    validate_project_name()
-    validate_repo_name()
-    validate_app_name()
-    validate_authors()
-    validate_app_version()
-except ValueError as err:
-    error_messages.append(err)
-    error = True
+validations = [ validate_git_base_url, 
+                validate_project_name,
+                validate_repo_name,
+                validate_app_name,
+                validate_authors,
+                validate_app_version
+            ]
 
+# allow to run all validations
+for fn in validations:
+    try:
+        fn()
+    except ValueError as err:
+        error_messages.append(err.args[0])
+        error = True
+
+# if any error, raise SystemExit(1)
 if error:
-    logging.error(error_messages, exc_info=True)
-    raise SystemExit(1) from error_messages
-
-### old code below
-# check {{ cookiecutter.git_base_url}}
-# def check_url(url):
-#     """Function to check URL"""
-#     try:
-#         result = urlparse(url)
-#         return all([result.scheme, result.netloc])
-#     except:
-#         return False
-
-# git_base_url = '{{ cookiecutter.git_base_url}}'
-# if (not check_url(git_base_url)):
-#     message = ("'{}' is not a valid URL! ".format(git_base_url) +
-#                "Please, check the 'git_base_url' input")
-#     print("[ERROR]: " + message)
-#     error = True
-#     error_messages.append(message)
-
-# # check {{ cookiecutter.__app_name }}
-# MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
-# app_name = '{{ cookiecutter.__app_name }}'
-# if (not re.match(MODULE_REGEX, app_name) or
-#     len(app_name) < 2):
-#     message = ("'{}' is not a valid Python module name! ".format(app_name) +
-#                "Please, check the 'project_name' input")
-#     print("[ERROR]: " + message)
-#     error = True
-#     error_messages.append(message)
-
-# if error:
-#     sys.exit("; ".join(error_messages))
+    #e_message = "; ".join(error_messages)
+    #logging.error(e_message, exc_info=True)
+    raise SystemExit(1)
