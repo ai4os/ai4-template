d195de00942f2d1157ec836c1f4f483753872e07
Valentin <valentin.kozlov@gmail.com>
2024-07-31 13:36:40 +0200

    feat: update post_gen_project to use logging

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -11,81 +11,76 @@
     Creates 'test' branch
     Switches back to 'main'
 """
+import logging
 import os
 import re
 import shutil
 import subprocess as subp
 import sys
 
-APP_REGEX = r'^[a-z][_a-z0-9]+$'
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
 repo_name = '{{ cookiecutter.__repo_name }}'
 default_branch = 'main'
 readme_file = 'README.md'
 
-app_name = '{{ cookiecutter.__app_name }}'
-if not re.match(APP_REGEX, app_name):
-    print("")
-    print("[ERROR]: %s is not a valid Python package name!" % app_name)
-    print("         Please, use low case and no dashes!")
-
-    # exits with status 1 to indicate failure
-    sys.exit(1)
-
-
 def git_ini(repo):
-    """ Function
-        Initializes Git repository
+    """ Function to initialize Git repository
     """
     gitrepo = ('{{ cookiecutter.git_base_url }}'.rstrip('/')
                 + "/" +  repo + '.git')
-    try:
-        os.chdir("../" + repo)
-        subp.call(["git", "init", "-b", default_branch])
-        subp.call(["git", "add", "."])
-        subp.call(["git", "commit", "-m", "initial commit"])
-        subp.call(["git", "remote", "add", "origin", gitrepo])
-      
-        # create test branch automatically
-        subp.call(["git", "checkout", "-b", "test"])
-        # adjust [Build Status] for the test branch
-        readme_content=[]
-        with open(readme_file) as f_old:
-            for line in f_old:
-                if "[![Build Status]" in line:
-                    line = re.sub("/main*", "/test", line)
-                readme_content.append(line)
 
-        with open(readme_file, "w") as f_new:
-            for line in readme_content:
-                f_new.write(line)
+    subp.call(["git", "init", "-b", default_branch])
+    subp.call(["git", "add", "."])
+    subp.call(["git", "commit", "-m", "initial commit"])
+    subp.call(["git", "remote", "add", "origin", gitrepo])
 
-        subp.call(["git", "commit", "-a", "-m", "update README.md for the BuildStatus"])
+    return gitrepo
 
-        # switch back to main
-        subp.call(["git", "checkout", "main"])
-    except OSError as os_error:
-        sys.stdout.write('[Error] Creating git repository failed for ' + repo + " !")
-        sys.stdout.write('[Error] {} '.format(os_error))
-        return "Error"
-    else:
-        return gitrepo
+
+def create_branch(branch):
+    """ Function to create an additional branch"""
+
+    # switch to 'main'
+    subp.call(["git", "checkout", default_branch])
+
+    # create new branch
+    subp.call(["git", "checkout", "-b", branch])
+    # adjust [Build Status] for the branch
+    readme_content=[]
+    with open(readme_file) as f_old:
+        for line in f_old:
+            if "[![Build Status]" in line:
+                line = re.sub("/main*", "/"+branch, line)
+            readme_content.append(line)
+
+    with open(readme_file, "w") as f_new:
+        for line in readme_content:
+            f_new.write(line)
+
+    subp.call(["git", "commit", "-a", "-m", "update README.md for the BuildStatus"])
+
+    # switch back to main
+    subp.call(["git", "checkout", default_branch])
 
 
 try:
     # initialize git repository
+    os.chdir("../" + repo_name)
     git_user_app = git_ini(repo_name)
+    create_branch("test")
+    create_branch("dev")
 
-    if "Error" not in git_user_app:
-        print()
-        print("[Info] {} was created successfully,".format(repo_name))
-        print("       Don't forget to create corresponding remote repository: {}".format(git_user_app))
-        print("       then you can do 'git push origin --all'")
-        print()
+    message = f"""
+*******************************************
+{repo_name} was created successfully.
+Don't forget to create corresponding remote repository: {git_user_app}
+then you can do 'git push origin --all'
+*******************************************
+"""
+    print(message)
+    logging.info(message)
+except Exception as err:
+    logging.error(f"While attempting to create git repository an error occurred! {err}", exc_info=True)
+    raise SystemExit(1) from err
 
-    sys.exit(0)
-except OSError as os_error:
-    sys.stdout.write(
-        'While attempting to create git repository an error occurred! '
-    )
-    sys.stdout.write('Error! {} '.format(os_error))
