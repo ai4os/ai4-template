7a11e346ce5882c0a1cd5d7421b241a76c7d0958
Borja Esteban <boressan@outlook.com>
2024-09-27 10:29:04 +0200

    add ai4-metadata collection to API metadata

--- {{ cookiecutter.__repo_name }}/{{ cookiecutter.__app_name }}/config.py
+++ {{ cookiecutter.__repo_name }}/{{ cookiecutter.__app_name }}/config.py
@@ -10,20 +10,34 @@ import logging
 import os
 from importlib import metadata
 
-# Get AI model metadata
+import yaml
+
+
+# Get AI model metadata from pyproject.toml
 API_NAME = "{{ cookiecutter.__app_name }}"
-API_METADATA = metadata.metadata(API_NAME)  # .json
+PROJECT_METADATA = metadata.metadata(API_NAME)  # .json
 
 # Fix metadata for emails from pyproject parsing
-_EMAILS = API_METADATA["Author-email"].split(", ")
+_EMAILS = PROJECT_METADATA["Author-email"].split(", ")
 _EMAILS = map(lambda s: s[:-1].split(" <"), _EMAILS)
-API_METADATA["Author-emails"] = dict(_EMAILS)
+PROJECT_METADATA["Author-emails"] = dict(_EMAILS)
 
 # Fix metadata for authors from pyproject parsing
-_AUTHORS = API_METADATA.get("Author", "").replace(", ", ",").split(",")
+_AUTHORS = PROJECT_METADATA.get("Author", "").replace(", ", ",").split(",")
 _AUTHORS = [] if _AUTHORS == [""] else _AUTHORS
-_AUTHORS += API_METADATA["Author-emails"].keys()
-API_METADATA["Authors"] = sorted(_AUTHORS)
+_AUTHORS += PROJECT_METADATA["Author-emails"].keys()
+PROJECT_METADATA["Authors"] = sorted(_AUTHORS)
+
+# Get ai4-metadata.yaml metadata
+AI4_METADATA_DIR = os.getenv(
+    "AI4_METADATA_DIR",
+    default=f"{os.getcwd()}/../{API_NAME}",
+)
+with open(f"{AI4_METADATA_DIR}/ai4-metadata.yml", "r", encoding="utf-8") as stream:
+    AI4_METADATA = yaml.safe_load(stream)
+
+# Merge metadata from pyproject.toml and ai4-metadata.yml
+API_METADATA = {**PROJECT_METADATA, **AI4_METADATA}
 
 # logging level across API modules can be setup via API_LOG_LEVEL,
 # options: DEBUG, INFO(default), WARNING, ERROR, CRITICAL
