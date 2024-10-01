5d6a06cf7fd93682d4b9409c4d704cd50e2b3a12
Borja Esteban <boressan@outlook.com>
2024-10-01 14:37:35 +0200

    add validate_docker_image to pre gen hook

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -18,11 +18,17 @@ import re
 import sys
 from urllib.parse import urlparse
 
+import requests
+import requests.exceptions
+
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 FOLDER_REGEX = r"^[a-zA-Z0-9_-]+$"
 MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
 EMAIL_REGEX = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
 APP_VERSION_REGEX = r"^\d+\.\d+\.\d+$"
+DOCKER_HUB_API = "https://registry.hub.docker.com/v2"
+REQ_TIMEOUT = 1000  # Milliseconds
+
 
 # -----------------------------------------------------------------------------
 def validate_git_base_url():
@@ -49,8 +55,10 @@ def validate_project_name():
         logging.error(e_message)
         raise ValueError(e_message)
 
+
 # repo_name and app_name are derived automatically in cookiecutter.json,
 # nevertheless, let's check them here
+
 
 def validate_repo_name():
     """Validate repo_name"""
@@ -66,8 +74,9 @@ def validate_app_name():
     app_name = "{{ cookiecutter.__app_name }}"
     if not re.match(MODULE_REGEX, app_name):
         e_message = f"Invalid package name ({app_name})"
-        logging.error(e_message )
+        logging.error(e_message)
         raise ValueError(e_message)
+
 
 # -----------------------------------------------------------------------------
 def validate_authors():
@@ -95,18 +104,54 @@ def validate_app_version():
         logging.error(e_message)
         raise ValueError(e_message)
 
+
+# -----------------------------------------------------------------------------
+def validate_docker_image():
+    """Validate docker image in docker hub"""
+    # Construct the URL for Docker Hub API v2
+    image = "{{ cookiecutter.docker_baseimage }}"
+    cpu_tag = "{{ cookiecutter.baseimage_tag }}"
+    gpu_tag = "{{ cookiecutter.baseimage_gpu_tag }}"
+
+    cpu_image_url = f"{DOCKER_HUB_API}/repositories/{image}/tags/{cpu_tag}"
+    try:
+        response = requests.get(cpu_image_url, timeout=REQ_TIMEOUT)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        e_message = f"Invalid docker image {image}:{cpu_tag}\n{err}"
+        logging.error(e_message)
+        raise ValueError(e_message)
+    except requests.exceptions.Timeout:
+        logging.warning(f"Timeout for {cpu_image_url}")
+        pass  # In case of network fail, continue
+
+    gpu_image_url = f"{DOCKER_HUB_API}/repositories/{image}/tags/{gpu_tag}"
+    try:
+        response = requests.get(gpu_image_url, timeout=REQ_TIMEOUT)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        e_message = f"Invalid docker image {image}:{cpu_tag}\n{err}"
+        logging.error(e_message)
+        raise ValueError(e_message)
+    except requests.exceptions.Timeout:
+        logging.warning(f"Timeout for {gpu_image_url}")
+        pass  # In case of network fail, continue
+
+
 # -----------------------------------------------------------------------------
 # Run all validations, exit with error if any
 # init error_messages
 error = False
 error_messages = []
-validations = [ validate_git_base_url, 
-                validate_project_name,
-                validate_repo_name,
-                validate_app_name,
-                validate_authors,
-                validate_app_version
-            ]
+validations = [
+    validate_git_base_url,
+    validate_project_name,
+    validate_repo_name,
+    validate_app_name,
+    validate_authors,
+    validate_app_version,
+    validate_docker_image,
+]
 
 # allow to run all validations
 for fn in validations:
@@ -118,6 +163,6 @@ for fn in validations:
 
 # if any error, raise SystemExit(1)
 if error:
-    #e_message = "; ".join(error_messages)
-    #logging.error(e_message, exc_info=True)
+    # e_message = "; ".join(error_messages)
+    # logging.error(e_message, exc_info=True)
     raise SystemExit(1)
