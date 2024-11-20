fa563fcbe512145deb4406caf30f8cfeef98a030
Borja Esteban <boressan@outlook.com>
2024-11-20 11:17:21 +0100

    fix metadata using only doc variables

--- {{ cookiecutter.__repo_name }}/{{ cookiecutter.__app_name }}/config.py
+++ {{ cookiecutter.__repo_name }}/{{ cookiecutter.__app_name }}/config.py
@@ -12,21 +12,9 @@ from importlib import metadata
 
 import yaml
 
-
 # Get AI model metadata from pyproject.toml
 API_NAME = "{{ cookiecutter.__app_name }}"
-PROJECT_METADATA = metadata.metadata(API_NAME)  # .json
-
-# Fix metadata for emails from pyproject parsing
-_EMAILS = PROJECT_METADATA["Author-email"].split(", ")
-_EMAILS = map(lambda s: s[:-1].split(" <"), _EMAILS)
-PROJECT_METADATA["Author-emails"] = dict(_EMAILS)
-
-# Fix metadata for authors from pyproject parsing
-_AUTHORS = PROJECT_METADATA.get("Author", "").split(", ")
-_AUTHORS = [] if _AUTHORS == [""] else _AUTHORS
-_AUTHORS += PROJECT_METADATA["Author-emails"].keys()
-PROJECT_METADATA["Authors"] = sorted(_AUTHORS)
+PACKAGE_METADATA = metadata.metadata(API_NAME)  # .json
 
 # Get ai4-metadata.yaml metadata
 CWD = os.getcwd()
@@ -38,11 +26,24 @@ if AI4_METADATA_DIR is None:
         AI4_METADATA_DIR = f"{CWD}/../{API_NAME}"
 
 # Open ai4-metadata.yml
-with open(f"{AI4_METADATA_DIR}/ai4-metadata.yml", "r", encoding="utf-8") as stream:
+_file = f"{AI4_METADATA_DIR}/ai4-metadata.yml"
+with open(_file, "r", encoding="utf-8") as stream:
     AI4_METADATA = yaml.safe_load(stream)
 
-# Add ai4-metadata to project metadata
-PROJECT_METADATA["AI4-Metadata"] = AI4_METADATA
+# Project metadata
+PROJECT_METADATA = {
+  "name": PACKAGE_METADATA["Name"],
+  "description": AI4_METADATA["description"],
+  "license": PACKAGE_METADATA["License"],
+  "author": PACKAGE_METADATA["Author-email"],
+  "version":  PACKAGE_METADATA["Version"],
+  "url":  PACKAGE_METADATA["Project-URL"],
+}
+
+# Fix metadata for emails from pyproject parsing
+_EMAILS = PACKAGE_METADATA["Author-email"].split(", ")
+_EMAILS = map(lambda s: s[:-1].split(" <"), _EMAILS)
+PROJECT_METADATA["author"] = dict(_EMAILS)
 
 # logging level across API modules can be setup via API_LOG_LEVEL,
 # options: DEBUG, INFO(default), WARNING, ERROR, CRITICAL
